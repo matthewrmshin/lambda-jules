592c31d43f21e8eb194bc9f0b80b2872c5a0f18e
Matt Shin <matthew.shin@metoffice.gov.uk>
2019-11-20 16:24:58 +0000

    Simplify scripts
    Build script now pulls from build image from Docker Hub.
    Lambda handler has better doc strings.

--- task/index.py
+++ task/index.py
@@ -1,45 +1,45 @@
-"""Handler that calls a binary executable on AWS lambda."""
+"""Wrap JULES to run under in the AWS Lambda Python 3.7 Runtime.
+
+Input: `event['body']` is expected to contain base64 encoded bytes that decode
+into a tar-gzip file containing the essential input files to JULES, including
+namelists, ancilliary files and forcing files. The tar-gzip archive should have
+all files in a flat directory at the root level. The output directory setting
+in the `output.nml` should be set to `./output`.
+
+Output: The response body will contain base64 encoded bytes that decode into a
+tar-gzip file containing the output files in a flat directory at the root
+level.
+"""
 
 
 from base64 import b64decode, b64encode
-import io
-import json
-import logging
+from io import BytesIO
 import os
-from subprocess import run, CalledProcessError
-import sys
+from subprocess import run
 import tarfile
 from tempfile import TemporaryDirectory
 
 
-logging.basicConfig(
-    datefmt='%Y%m%dT%H%M%S%z',
-    format='%(asctime)s [%(levelname)s] %(message)s',
-    level=logging.INFO,
-)
-LOG = logging.getLogger()
-LOG.setLevel(logging.INFO)
+def handler(event: dict, context) -> dict:
+    """Handle the lambda call.
 
-
-def handler(event, context):
-    """Handle the lambda call."""
+    Create and change into a temporary directory.
+    Unpack the input there.
+    Invoke JULES.
+    Pack the output and return.
+    """
     # Run in a temporary location to guarantee writable
-    LOG.info('event=%s', event)
     tempdir = TemporaryDirectory()
     os.chdir(tempdir.name)
     os.makedirs('output')
-    extract_input(event)
+    unpack_input(event)
     # Set up executable and shared library path
     mybin = os.path.join(os.path.dirname(__file__), 'bin', 'jules.exe')
     try:
         run([mybin], check=True)
-    except CalledProcessError as exc:
-        LOG.exception(exc)
-        raise
-    else:
         return {
             'statusCode': 200,
-            'body': get_output(),
+            'body': pack_output(),
             'headers': {'content-type': 'application/octet-stream'},
             'isBase64Encoded': True,
         }
@@ -48,28 +48,28 @@ def handler(event, context):
         tempdir.cleanup()
 
 
-def main():
-    """CLI for testing."""
-    event = None
-    if sys.argv[1:]:
-        event = json.loads(sys.argv[1])
-    handler(event, None)
+def unpack_input(event: dict) -> None:
+    """Unpack the input bytes in `event['body']` into the current directory.
 
-
-def extract_input(event):
-    """Get input from payload. Assume content is a tar(-gzip) archive."""
-    with tarfile.open(fileobj=io.BytesIO(b64decode(event['body']))) as handle:
+    `event['body']` is expected to contain base64 encoded bytes that decode
+    into a tar-gzip file containing the essential input files to JULES,
+    including namelists, ancilliary files and forcing files. The tar-gzip
+    archive should have all files in a flat directory at the root level. The
+    output directory setting in the `output.nml` should be set to `./output`.
+    """
+    with tarfile.open(fileobj=BytesIO(b64decode(event['body']))) as handle:
         handle.extractall()
 
 
-def get_output():
-    """Read output, turning it into a string containing a tar-gzip archive."""
-    ret = io.BytesIO()
+def pack_output() -> str:
+    """Pack the content of the output directory.
+
+    :return: The response body will contain a string that is a base64 encoded
+    bytes that can be decoded into a tar-gzip file containing the output files
+    in a flat directory at the root level.
+    """
+    ret = BytesIO()
     with tarfile.open(fileobj=ret, mode='w:gz') as handle:
         for name in os.listdir('output'):
             handle.add(os.path.join('output', name), name)
     return b64encode(ret.getvalue()).decode()
-
-
-if __name__ == '__main__':
-    main()
